822033b5fcdb53f9e672b17b56428a81539da749
banteg <4562643+banteg@users.noreply.github.com>
2024-05-04 03:46:55 +0400

    fix: bigint serialization in persister

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { experimental_createPersister } from "@tanstack/react-query-persist-client";
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { WagmiProvider } from "wagmi";
+import { WagmiProvider, deserialize, serialize } from "wagmi";
 
 import App from "./App.tsx";
 import { config } from "./wagmi.ts";
@@ -21,12 +21,15 @@ globalThis.Buffer = Buffer;
 const persister = experimental_createPersister({
   storage: AsyncStorage,
   maxAge: 1000 * 60 * 60 * 12,
+  serialize: serialize,
+  deserialize: deserialize,
 });
 const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
       retry: 0,
       persister: persister,
+      gcTime: 1000 * 60 * 60 * 24,
     },
   },
 });
