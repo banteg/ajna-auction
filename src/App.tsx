2e87c3c466b844daf0c34f3425aa5c76dfdbe327
banteg <4562643+banteg@users.noreply.github.com>
2024-05-03 16:56:26 +0400

    feat: ajna auction tracker

--- src/App.tsx
+++ src/App.tsx
@@ -1,46 +1,133 @@
-import { useAccount, useConnect, useDisconnect } from 'wagmi'
+import { Box, Container, Flex, Grid, Strong, Text, TextField } from "@radix-ui/themes";
+import { queryOptions, useQueryClient } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { useEffect, useState } from "react";
+import { formatUnits } from "viem";
+import { useAccount, useBlockNumber, useConnect, useDisconnect } from "wagmi";
+import { useReadPoolInfoUtilsAuctionStatus } from "./generated";
 
-function App() {
-  const account = useAccount()
-  const { connectors, connect, status, error } = useConnect()
-  const { disconnect } = useDisconnect()
-
-  return (
-    <>
-      <div>
-        <h2>Account</h2>
-
-        <div>
-          status: {account.status}
-          <br />
-          addresses: {JSON.stringify(account.addresses)}
-          <br />
-          chainId: {account.chainId}
-        </div>
-
-        {account.status === 'connected' && (
-          <button type="button" onClick={() => disconnect()}>
-            Disconnect
-          </button>
-        )}
-      </div>
-
-      <div>
-        <h2>Connect</h2>
-        {connectors.map((connector) => (
-          <button
-            key={connector.uid}
-            onClick={() => connect({ connector })}
-            type="button"
-          >
-            {connector.name}
-          </button>
-        ))}
-        <div>{status}</div>
-        <div>{error?.message}</div>
-      </div>
-    </>
-  )
+export function format_wei(value: bigint, decimals: number, digits = 5) {
+  // format to significant digits while keeping integer precision
+  const [integer_part, _decimal_part] = formatUnits(value, decimals).split(".");
+  const significant_digits = Math.max(integer_part.length, digits);
+  return (Number(value) / 10 ** decimals).toLocaleString("en-US", {
+    maximumSignificantDigits: Math.min(significant_digits, 18),
+  });
 }
 
-export default App
+function Input({ label, value, set_value }) {
+  return (
+    <Box>
+      <Text color="gray">{label}</Text>
+      <TextField.Root placeholder={label} value={value} onChange={(e) => set_value(e.target.value)}></TextField.Root>
+    </Box>
+  );
+}
+
+function AjnaAuctionInfo({ query }) {
+  console.log(query.status);
+  if (query.isError) {
+    return (
+      <Box>
+        <Text color="red">{query.error.name}</Text>
+        <Text size="1" color="red" style={{ whiteSpace: "pre-wrap" }}>
+          {query.error.message}
+        </Text>
+      </Box>
+    );
+  }
+  if (!query.isSuccess) return;
+  const kick_time = new Date(Number.parseInt(query.data[0].toString()) * 1000)
+    .toISOString()
+    .replace("T", " ")
+    .replace(".000", "");
+  return (
+    <Flex direction="column" gap="2">
+      {query.isFetching ? (
+        <Text color="blue" size="1">
+          ● fetching…
+        </Text>
+      ) : (
+        <Text color="green" size="1">
+          ● updated
+        </Text>
+      )}
+      <Grid columns="2" align="baseline" gap="2">
+        <Text>uint256 kickTime_</Text>
+        <Text size="2" truncate>
+          {kick_time}
+        </Text>
+        <Text>uint256 collateral_,</Text>
+        <Text truncate size="4">
+          {format_wei(query.data[1], 18)}
+        </Text>
+        <Text>uint256 debtToCover_,</Text>
+        <Text truncate size="4">
+          {format_wei(query.data[2], 18)}
+        </Text>
+        <Text>bool isCollateralized_,</Text>
+        <Text truncate size="4">
+          {query.data[3].toString()}
+        </Text>
+        <Text color="red">uint256 price_,</Text>
+        <Text truncate size="4" color="red">
+          <Strong>{format_wei(query.data[4], 18)}</Strong>
+        </Text>
+        <Text>uint256 neutralPrice_,</Text>
+        <Text truncate size="4">
+          {format_wei(query.data[5], 18)}
+        </Text>
+        <Text>uint256 referencePrice_,</Text>
+        <Text truncate size="4">
+          {format_wei(query.data[6], 18)}
+        </Text>
+        <Text>uint256 debtToCollateral_,</Text>
+        <Text truncate size="4">
+          {format_wei(query.data[7], 18)}
+        </Text>
+        <Text>uint256 bondFactor_</Text>
+        <Text truncate size="4">
+          {format_wei(query.data[8], 18)}
+        </Text>
+      </Grid>
+    </Flex>
+  );
+}
+
+function App() {
+  const query_client = useQueryClient();
+  const [pool, set_pool] = useState("0x07aaa9e40323a85e763a5b2eb9d8ca7ebaf7fb5a");
+  const [borrower, set_borrower] = useState("0x286ee152779ca230a30337d3f96df7963e41a307");
+  const { data: block_number } = useBlockNumber({ watch: true });
+  const auction_query = useReadPoolInfoUtilsAuctionStatus({
+    args: [pool, borrower],
+    query: { enabled: pool && borrower && block_number, retry: 0 },
+  });
+  console.log(auction_query);
+
+  useEffect(() => {
+    query_client.invalidateQueries({ queryKey: auction_query.queryKey });
+  }, [block_number]);
+
+  return (
+    <Container size="1" p="2rem">
+      <Flex direction="column" gap="4">
+        <Flex align="baseline">
+          <Text size="5">ajna auction</Text>
+          <Box flexGrow="1" />
+          {block_number && (
+            <Text size="2" color="gray">
+              block {block_number?.toLocaleString("en-US")}
+            </Text>
+          )}
+        </Flex>
+        <Input label="pool" value={pool} set_value={set_pool} />
+        <Input label="borrower" value={borrower} set_value={set_borrower} />
+        <AjnaAuctionInfo query={auction_query} />
+      </Flex>
+      <ReactQueryDevtools />
+    </Container>
+  );
+}
+
+export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,24 +1,30 @@
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { Buffer } from 'buffer'
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { WagmiProvider } from 'wagmi'
+import { Buffer } from "buffer";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { WagmiProvider } from "wagmi";
 
-import App from './App.tsx'
-import { config } from './wagmi.ts'
+import App from "./App.tsx";
+import { config } from "./wagmi.ts";
 
-import './index.css'
+import "./index.css";
 
-globalThis.Buffer = Buffer
+import "@radix-ui/themes/styles.css";
 
-const queryClient = new QueryClient()
+import { Theme } from "@radix-ui/themes";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+globalThis.Buffer = Buffer;
+
+const queryClient = new QueryClient();
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <WagmiProvider config={config}>
       <QueryClientProvider client={queryClient}>
-        <App />
+        <Theme>
+          <App />
+        </Theme>
       </QueryClientProvider>
     </WagmiProvider>
   </React.StrictMode>,
-)
+);
