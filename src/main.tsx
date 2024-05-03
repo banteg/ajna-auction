b099bab5f62b6cb5f092e7f0d40e8743f1329df1
banteg <4562643+banteg@users.noreply.github.com>
2024-05-04 02:46:59 +0400

    feat: persistence

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,8 @@
 import { Buffer } from "buffer";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+import { experimental_createPersister } from "@tanstack/react-query-persist-client";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { WagmiProvider } from "wagmi";
@@ -15,7 +18,18 @@ import { Theme } from "@radix-ui/themes";
 
 globalThis.Buffer = Buffer;
 
-const queryClient = new QueryClient();
+const persister = experimental_createPersister({
+  storage: AsyncStorage,
+  maxAge: 1000 * 60 * 60 * 12,
+});
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: 0,
+      persister: persister,
+    },
+  },
+});
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
