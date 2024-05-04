272e5fc1e9f358c58328aa793b3f83ca9e3810ac
banteg <4562643+banteg@users.noreply.github.com>
2024-05-04 16:09:29 +0400

    feat: load pools and keep the list fresh

--- src/components/AjnaPools.tsx
+++ src/components/AjnaPools.tsx
@@ -10,36 +10,41 @@ import { config } from "../wagmi";
 
 const ajna_factory: Address = "0x6146DD43C5622bB6D12A5240ab9CF4de14eDC625";
 
-export function AjnaPools() {
-  const query_client = useQueryClient();
-  const page_size = 100000n;
-  //   const start_block = 18962313n;
-  const start_block = 18962000n;
-  //   const [end_block, set_block] = useState();
-  const { data: end_block } = useBlockNumber();
-  const num_pages = end_block && (end_block - start_block + page_size) / page_size;
-  const [pools, set_pools] = useState([]);
-  const [sel_page, set_page] = useState(0);
+interface PageParam {
+  from_block: bigint;
+  to_block: bigint;
+}
 
-  async function fetch_logs({ pageParam }) {
+export function AjnaPools() {
+  const page_size = 100000n;
+  const start_block = 18962313n;
+  const { data: end_block_static } = useBlockNumber(); // static block for pools
+
+  async function fetch_logs({ pageParam }: { pageParam: PageParam }) {
     // fetches PoolCreated and extracts a list of pools
-    // TODO fetch last page
-    console.log("fetch page", pageParam);
-    const logs = await getLogs(config.getClient(), {
+    console.log("fetch page", pageParam, "size", pageParam.to_block - pageParam.from_block + 1n);
+    return getLogs(config.getClient(), {
       address: ajna_factory,
       event: parseAbiItem("event PoolCreated(address pool, bytes32 subsetHash)"),
-      fromBlock: pageParam,
-      toBlock: pageParam + page_size - 1n,
+      fromBlock: pageParam.from_block,
+      toBlock: pageParam.to_block,
     });
-    return logs;
   }
 
   const pools_query = useInfiniteQuery({
-    queryKey: ["ajna_pools_dev"],
+    queryKey: ["ajna_pools_dev_2", ajna_factory],
     queryFn: fetch_logs,
-    initialPageParam: start_block,
-    getNextPageParam: (lastPage, allPages, lastPageParam, allPageParams) =>
-      lastPageParam + page_size < end_block ? lastPageParam + page_size : null,
+    initialPageParam: { from_block: start_block, to_block: start_block + page_size - 1n },
+    getNextPageParam: (lastPage, allPages, lastPageParam, allPageParams) => {
+      if (!end_block_static) return undefined;
+      const next_page: PageParam = {
+        from_block: lastPageParam.to_block + 1n,
+        to_block: lastPageParam.to_block + page_size,
+      };
+      if (next_page.from_block > end_block_static) return null;
+      if (next_page.to_block > end_block_static) next_page.to_block = end_block_static;
+      return next_page;
+    },
     select: (data) => ({
       num_pages: data.pages.length,
       pools: data.pages.flat().map((log) => log.args.pool),
@@ -51,27 +56,20 @@ export function AjnaPools() {
   });
 
   useEffect(() => {
-    console.log("num pages changed", pools_query.data?.num_pages, pools_query.fetchStatus, pools_query.status);
     if (pools_query.isFetching) return;
     if (pools_query.hasNextPage) pools_query.fetchNextPage();
-  }, [pools_query.data?.num_pages]);
-
-  if (pools_query.isFetching || pools_query.hasNextPage) {
-    return <Text color="blue">fetching ajna pools, found {pools_query.data?.pools?.length} pools</Text>;
-  }
-
-  console.log(pools_query.data);
+  }, [pools_query.data?.num_pages, end_block_static]);
 
   return (
     <Flex direction="column" gap="2">
-      <Text size="5" color="purple">
-        anja pools {pools_query.data?.num_pages} / {num_pages?.toString()}, num_pools ={" "}
-        {pools_query.data?.pools?.length}
-      </Text>
-      {pools_query.hasNextPage && <Button onClick={(e) => pools_query.fetchNextPage()}>fetch more</Button>}
-      <Text>start block: {start_block.toString()}</Text>
-      <Text>end block: {end_block?.toString()}</Text>
-      <Text size="1">{JSON.stringify(pools_query.data?.deploy_blocks)}</Text>
+      <Box>
+        {pools_query.isFetching || pools_query.hasNextPage ? (
+          <Text color="blue">fetching ajna pools, found {pools_query.data?.pools?.length ?? "no"} pools</Text>
+        ) : (
+          <Text color="green">loaded {pools_query.data?.pools?.length} ajna pools</Text>
+        )}
+      </Box>
+      <Text size="1">{JSON.stringify(pools_query.data?.pools)}</Text>
     </Flex>
   );
 }
