48cfe040350ba26a4c465a35b6768898cb34184f
banteg <4562643+banteg@users.noreply.github.com>
2024-05-06 03:00:02 +0400

    feat: auction price chart

--- src/components/Chart.tsx
+++ src/components/Chart.tsx
@@ -1,4 +1,9 @@
+import { Box, Flex, Text } from "@radix-ui/themes";
+import { useMemo } from "react";
 import { Line, LineChart, ReferenceLine, Tooltip, XAxis, YAxis } from "recharts";
+import { serialize } from "wagmi";
+import { auction_price } from "../ajna";
+import { format_wad, range } from "../utils";
 
 export function Chart({ data }: { data: { x: number; y: number }[] }) {
   const last = data[data.length - 1];
@@ -21,5 +26,41 @@ export function Chart({ data }: { data: { x: number; y: number }[] }) {
         />
       </LineChart>
     </div>
+  );
+}
+
+export function AuctionPriceChart({ refernce_price, kick_time }) {
+  const referenece_float = Number.parseFloat(format_wad(refernce_price));
+  const data = useMemo(() => {
+    const minutes = range(72 * 60);
+    return minutes.map((elapsed) => ({ x: elapsed, y: auction_price(referenece_float, elapsed) }));
+  }, [refernce_price]);
+
+  if (!refernce_price || !kick_time) return;
+
+  const elapsed_minutes = Number.parseInt((BigInt(new Date()) / 1000n - kick_time) / 60n);
+  console.log(elapsed_minutes);
+
+  return (
+    <Box>
+      <Flex direction="column" gap="2">
+        <Text size="3">auction chart</Text>
+        <LineChart
+          style={{ fontSize: "12px" }}
+          width={500}
+          height={200}
+          data={data.slice(
+            Math.max(0, elapsed_minutes - 60),
+            Math.min(elapsed_minutes + 3 * 60, 72 * 60),
+          )}
+        >
+          <XAxis type="number" dataKey="x" domain={["dataMin", "dataMax"]} />
+          <YAxis type="number" dataKey="y" />
+          <Line type="linear" dot={false} dataKey="y" isAnimationActive={false} />
+          <ReferenceLine x={elapsed_minutes} stroke="red" />
+          <ReferenceLine y={referenece_float} stroke="red" />
+        </LineChart>
+      </Flex>
+    </Box>
   );
 }
