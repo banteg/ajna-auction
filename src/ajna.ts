2c560a3bc1ecb90810b373ddc4ddbd2f76b31d4e
banteg <4562643+banteg@users.noreply.github.com>
2024-05-06 02:59:50 +0400

    feat: auction price formula

--- src/ajna.ts
+++ src/ajna.ts
@@ -1,8 +1,23 @@
 const liquidation_events = ["Kick", "Take", "BucketTake", "Settle", "AuctionSettle"];
 
 export function get_auction_data(logs) {
+  if (!logs) return;
   let auctions = logs?.filter((log) => liquidation_events.includes(log.eventName));
   auctions = Object.groupBy(auctions, (el, i) => el.args.borrower);
   console.log(Object.keys(auctions));
   return auctions;
+}
+
+export function auction_price(reference_price: number, elapsed_minutes: number): number {
+  // my approximation of ajna auction formula
+  if (elapsed_minutes < 120) {
+    const time_adj = (-1 * elapsed_minutes) / 20;
+    return 256 * reference_price * 2 ** time_adj;
+  }
+  if (elapsed_minutes < 840) {
+    const time_adj = (-1 * (elapsed_minutes - 120)) / 120;
+    return 4 * reference_price * 2 ** time_adj;
+  }
+  const time_adj = (-1 * (elapsed_minutes - 840)) / 60;
+  return (reference_price * 2 ** time_adj) / 16;
 }
